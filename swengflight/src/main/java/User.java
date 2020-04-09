c2fbf304863abbb729e363c154345c854ac4f706
TimMarotta <timothy.marotta@yahoo.com>
2020-04-09 16:55:12 -0400

    refactor of exportData as JSON output format

--- swengflight/src/main/java/User.java
+++ swengflight/src/main/java/User.java
@@ -1,12 +1,12 @@
-import java.io.IOException;
-import java.io.PrintStream;
+import java.io.*;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class User {
+public class User implements Serializable {
 
     LinkedList<Trip> trips;
     String id;//can't be empty string
@@ -84,44 +84,18 @@ public class User {
     }
 
     /**
-     * exports data from User object to .txt file
-     * @param filename name of .txt file
+     * exports data from User object to JSON file
+     * @param filename name of JSON file
      */
-    public void exportData(String filename) throws IOException {
-        PrintStream printStream = new PrintStream(filename);
-        DateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy HH:mm:ss");
+    public void exportData(String filename){
+        ObjectMapper mapper = new ObjectMapper();
+        File output = new File(filename +".json");
 
-        //print user ID and time of export
-        printStream.println("User ID: " + getId());
-        printStream.println("Time of export: " + dateFormat.format(new Date()));
-        printStream.println();
-
-        //print flights associated with each trip
-        for (int i = 0; i < trips.size(); i++){
-            Trip nextTrip = trips.get(i);
-            Collection<Ticket> tickets = nextTrip.getTickets();
-            for(Ticket next: tickets){
-                printStream.println("Passenger name: " + next.getName());
-                printStream.println("Airline: " + next.getAirline());
-                printStream.println("Ticket Number: " + next.getTicketNumber());
-                printStream.println("Date: " + next.getDate());
-                printStream.println("Flight: " + next.getFlight());
-                Flight nextFlight = next.getFlight();
-                printStream.println("\tAirport: " + nextFlight.getAirport());
-                printStream.println("\tGate: " + nextFlight.getGate());
-                printStream.println("\tArrival City: " + nextFlight.getArrivalCity());
-                printStream.println("\tDeparture City: " + nextFlight.getDepartureCity());
-                printStream.println("\tBoarding Time: " + nextFlight.getBoardingTime());
-                printStream.println("\tDeparture Time: " + nextFlight.getDepartureTime());
-                printStream.println("\tArrival Time: " + nextFlight.getArrivalTime());
-                printStream.println();
-            }
-            printStream.println("---");
-            printStream.println();
-            printStream.flush();
+        try {
+            mapper.writeValue(output, this);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        //close file
-        printStream.close();
     }
   
     public static String getDateString(ZonedDateTime date){
