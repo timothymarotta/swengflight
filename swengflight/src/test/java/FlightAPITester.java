ae21fa84c00bea677e8b7298bb00a7df90edc3ce
Saul Amster <saulamster@gmail.com>
2020-04-20 14:32:06 -0400

    added basic caching and implementation of aviation-stack flight api

---
+++ swengflight/src/test/java/FlightAPITester.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class FlightAPITester {
+
+    @Test
+    public void getCredentialsTest() {
+        String access_key = FlightAPI.getKey();
+        System.out.println(access_key);
+        assertTrue(access_key != null);
+    }
+
+    @Test
+    public void testGetFlightByNumber() throws Exception {
+        FlightAPI.getFlightByNumber("1161");
+    }
+}
