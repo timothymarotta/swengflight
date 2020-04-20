cf0d049bf21776d21bade61c2785dfe2f40ec5f7
JoshHayden <jhayden@ithaca.edu>
2020-04-20 14:07:56 -0400

    Adding my default user for testing, going to merge master now so I can get the file IO

--- swengflight/src/main/java/Flight_UI.java
+++ swengflight/src/main/java/Flight_UI.java
@@ -1,3 +1,7 @@
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Collection;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 enum FlightState {Landing, ViewTrips, GetFlightInfo, AddTrip, AddFlightToTrip}
@@ -45,5 +49,15 @@ public class Flight_UI {
     }
     static void handleAddFlightToTrip(){
 
+    }
+    //Create basic user for testing
+    static void testingUser(){
+        User user = new User("Test");
+        Collection<Ticket> tickets1 = new LinkedList<Ticket>();
+        Flight houstonToMiami = new Flight("IAH", "A23", "MIA", "Houston", LocalDateTime.of(2020, 4, 12, 9, 25).atZone(ZoneId.of("America/Chicago")), LocalDateTime.of(2020, 4, 12, 9, 55).atZone(ZoneId.of("America/Chicago")), LocalDateTime.of(2020, 4, 12, 11, 45).atZone(ZoneId.of("America/Puerto_Rico")));
+        tickets1.add(new Ticket("Josh Hayden", "American", "1234", houstonToMiami, LocalDateTime.of(2020, 4, 12, 0, 0).atZone(ZoneId.of("America/Chicago"))));
+        Flight miamiToHouston = new Flight("MIA", "B13", "IAH", "Miami",  LocalDateTime.of(2020, 4, 19, 8, 30).atZone(ZoneId.of("America/Puerto_Rico")), LocalDateTime.of(2020, 4, 19, 9, 0).atZone(ZoneId.of("America/Puerto_Rico")), LocalDateTime.of(2020, 4, 19, 12, 30).atZone(ZoneId.of("America/Puerto_Rico")));
+        tickets1.add(new Ticket("Josh Hayden", "American", "2345", miamiToHouston, LocalDateTime.of(2020, 4, 19, 0, 0).atZone(ZoneId.of("America/Chicago"))));
+        Trip trip1 = new Trip(tickets1);
     }
 }
